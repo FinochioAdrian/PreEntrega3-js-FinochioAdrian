de713915cdf0370e773b62f98e892bad902d97b8
FinochioAdrian <eidrienhez33@gmail.com>
2023-09-19 00:04:56 -0300

    [FIX] resolucion de conflicto con pathName de:  main.js

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ import tienda from "./pages/tienda.js";
 
 
 const pathName = window.location.pathname
-console.log(pathName)
-switch (pathName) {
-    case "/index.html":
+const arrPathname =pathName.split("/")
+const lastPathName =arrPathname[arrPathname.length-1]
+switch (lastPathName) {
+    case "index.html":
         index()
       break;
-    case "/tienda.html":
+    case "tienda.html":
     tienda()
-    console.log("hola que pasa??");
         break;
-    case "/cart.html":
+    case "cart.html":
     cart()
         break;
-    case "/productos.html":
+    case "productos.html":
     productos()
         break;
 
